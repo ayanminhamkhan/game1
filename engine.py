9bc05900f3e2be4b08d4e06347fc80a507b7d0f8
ayanminhamkhan <ayanminhamkhan77@gmail.com>
2023-12-17 12:56:21 +0530

    part 2.9

--- engine.py
+++ engine.py
@@ -3,7 +3,6 @@ from typing import Set,Iterable,Any
 from tcod.context import Context
 from tcod.console import Console
 
-from actions import EscapeAction,MovementAction
 from entity import Entity
 from input_handler import EventsHandler
 from game_map import GameMap
@@ -23,12 +22,7 @@ class Engine:
             if action == None:
                 continue
 
-            if isinstance(action,MovementAction):
-                if self.game_map.tiles["walkable"][self.player.x + action.dx,self.player.y + action.dy]:
-                    self.player.move(dx= action.dx, dy= action.dy)
-
-            if isinstance(action,EscapeAction):
-                raise SystemExit()
+            action.perform(self,self.player)
             
     def render(self,console: Console, context: Context)-> None:
         self.game_map.render(console)
